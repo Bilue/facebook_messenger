12de45fe5d21599583fb56931a444c51a5119949
Simon Westren <simonwestren@gmail.com>
2016-09-20 19:13:52 +1000

    Adding in ability to send generic message templates and actions from bot

--- lib/message_sender.ex
+++ lib/message_sender.ex
@@ -13,10 +13,46 @@ defmodule FacebookMessenger.Sender do
   @spec send(String.t, String.t) :: HTTPotion.Response.t
   def send(recepient, message) do
     Logger.debug(url)
-    Logger.debug(json_payload(recepient, message))
+    Logger.debug(message_payload(recepient, message))
     res = manager.post(
       url: url,
-      body: json_payload(recepient, message)
+      body: json_payload(message_payload(recepient, message))
+    )
+    Logger.info("response from FB #{inspect(res)}")
+    res
+  end
+
+  @doc """
+  sends a generic template to the recepient
+
+    * :recepient - the recepient to send the message to
+    * :elements - contains the elements to send 
+  """
+  @spec send(String.t, Array.t) :: HTTPotion.Response.t
+  def send(recepient, elements) do
+    Logger.debug(url)
+    Logger.debug(template_payload(recepient, elements))
+    res = manager.post(
+      url: url,
+      body: json_payload(template_payload(recepient, elements))
+    )
+    Logger.info("response from FB #{inspect(res)}")
+    res
+  end
+
+  @doc """
+  sends a sender action to the recipient
+
+    * :recipient - the recipient to send the message to
+    * :action - contains a action to the user i.e. "mark_seen" to mark last message as read
+  """
+  @spec send_action(String.t, String.t) :: HTTPotion.Response.t
+  def send_action(recipient, action) do
+    Logger.debug(url)
+    Logger.debug(action_payload(recipient, action))
+    res = manager.post(
+      url: url,
+      body: json_payload(action_payload(recipient, action))
     )
     Logger.info("response from FB #{inspect(res)}")
     res
@@ -25,25 +61,49 @@ defmodule FacebookMessenger.Sender do
   @doc """
   creates a payload to send to facebook
 
-    * :recepient - the recepient to send the message to
+    * :recipient - the recipient to send the message to
     * :message - the message to send
   """
-  def payload(recepient, message) do
+  def message_payload(recipient, message) do
     %{
-      recipient: %{id: recepient},
+      recipient: %{id: recipient},
       message: %{text: message}
+    }
+  end
+
+  @doc """
+  creates a template payload to send to facebook
+
+    * :recipient: - the recipient to send the message to
+    * :elements - contains the elements to send
+  """
+  def template_payload(recipient, elements) do
+    %{
+      recipient: %{id: recipient},
+      message: %{attachment: %{type: "template", payload: %{template_type: "generic", elements: elements}}}
+    }
+  end
+
+  @doc """
+  creates an action payload to send to facebook
+
+    * :recipient - the recipient to send the message to
+    * :action - the sender action string
+  """
+  def action_payload(recipient, action) do
+    %{
+      recipient: %{id: recipient},
+      sender_action: action
     }
   end
 
   @doc """
   creates a json payload to send to facebook
 
-    * :recepient - the recepient to send the message to
-    * :message - the message to send
+    * :payload - a map of data to be json encoded
   """
-  def json_payload(recepient, message) do
-    payload(recepient, message)
-    |> Poison.encode
+  def json_payload(payload) do
+    Poison.encode(payload)
     |> elem(1)
   end
 
--- test/message_sender_test.exs
+++ test/message_sender_test.exs
@@ -7,12 +7,13 @@ defmodule TestBotOne.MessageSenderTest do
   end
 
   test "creates a correct payload" do
-    assert FacebookMessenger.Sender.payload(1055439761215256, "Hello") ==
+    assert FacebookMessenger.Sender.message_payload(1055439761215256, "Hello") ==
     %{message: %{text: "Hello"}, recipient: %{id: 1055439761215256}}
   end
 
   test "creates a correct payload in json" do
-    assert FacebookMessenger.Sender.json_payload(1055439761215256, "Hello") ==
+    payload = FacebookMessenger.Sender.message_payload(1055439761215256, "Hello")
+    assert FacebookMessenger.Sender.json_payload(payload) ==
     "{\"recipient\":{\"id\":1055439761215256},\"message\":{\"text\":\"Hello\"}}"
   end
 
@@ -20,4 +21,15 @@ defmodule TestBotOne.MessageSenderTest do
     FacebookMessenger.Sender.send(1055439761215256, "Hello")
     assert_received %{body: "{\"recipient\":{\"id\":1055439761215256},\"message\":{\"text\":\"Hello\"}}", url: "https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_TOKEN"}
   end
+
+  test "creates a correct generic template message" do
+    element = %{title: "foo", subtitle: "bar", item_url: "https://www.imaurl.com", image_url: "https://www.imaimageurl.com", buttons: [%{type: "postback", title: "Postback", payload: "First bubble payload"}]}
+    assert FacebookMessenger.Sender.template_payload(1055439761215256, [element]) == 
+    %{recipient: %{id: 1055439761215256}, message: %{attachment: %{type: "template", payload: %{template_type: "generic", elements: [element]}}}}
+  end
+
+  test "creates a correct generic template message in json" do
+    payload = FacebookMessenger.Sender.template_payload(1055439761215256, [])
+  end
+
 end
